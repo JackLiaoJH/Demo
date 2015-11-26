bfee31e2bda009f5cab124c0bb15d39cceadf259
LiaoJH <583125288@qq.com>
2015-11-26 23:42:20 +0800

    加载文字的添加

--- loadingdemo/src/main/java/com/liaojh/loadingdemo/MainActivity.java
+++ loadingdemo/src/main/java/com/liaojh/loadingdemo/MainActivity.java
@@ -1,15 +1,67 @@
 package com.liaojh.loadingdemo;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.liaojh.loadingdemo.widget.ProgressRoundView;
+
+import java.lang.ref.WeakReference;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressRoundView progressRoundView;
+
+    private Handler mHandler = new MyHandler(this);
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressRoundView = (ProgressRoundView) findViewById(R.id.prv);
+
+        progressRoundView.setProgress(0);
+        progressRoundView.setMaxProgress(15);
+        mHandler.sendEmptyMessage(1);
+
+    }
+
+    private int progress = 0;
+    private static class MyHandler extends Handler {
+        private WeakReference<Activity> reference;
+
+        public MyHandler(Activity activity) {
+            reference = new WeakReference<Activity>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Activity activity = reference.get();
+
+            if (activity != null && activity instanceof MainActivity) {
+                MainActivity mainActivity = (MainActivity) activity;
+
+                switch (msg.what) {
+                    case 1:
+//                        mainActivity.progressRoundView.increateProgress();
+                        mainActivity.progressRoundView.setProgress(mainActivity.progress ++);
+                        mainActivity.mHandler.sendEmptyMessageDelayed(1, 1000);
+                        break;
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacksAndMessages(null);
     }
 }
 
--- loadingdemo/src/main/java/com/liaojh/loadingdemo/widget/ProgressRoundView.java
+++ loadingdemo/src/main/java/com/liaojh/loadingdemo/widget/ProgressRoundView.java
@@ -4,8 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.RectF;
-import android.graphics.drawable.ShapeDrawable;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -19,7 +19,10 @@ public class ProgressRoundView extends View {
     private Paint mOutCirclePaint;
     private Paint mInCirclePaint;
     private Paint mArcPaint;
+    private Paint mTextPaint;
+
     private RectF mArcRectF;
+    private Rect mTextRect;
 
     private float mCircleX = 200, mCircleY = 200;
     private float mInCircleRadius = 70;
@@ -27,7 +30,10 @@ public class ProgressRoundView extends View {
 
     private float mArcLeft, mArcTop, mArcRight, mArcBottom;
 
-    private float mStartAngle = 0, mSweepAngle = 220;
+    private float mStartAngle = 0, mSweepAngle = 0;
+
+    private String mProgressText = "0%";
+    private int mMaxProgress = 360;
 
     public ProgressRoundView(Context context) {
         this(context, null);
@@ -59,6 +65,14 @@ public class ProgressRoundView extends View {
         mArcPaint.setStrokeWidth(mOutCircleRadius - mInCircleRadius);
         mArcPaint.setColor(Color.GREEN);
 
+        mTextPaint = new Paint();
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setColor(Color.BLACK);
+        mTextPaint.setTextSize(25);
+
+        mTextRect = new Rect();
+        mTextPaint.getTextBounds(mProgressText, 0, mProgressText.length(), mTextRect);
+
         float ds = (mOutCircleRadius - mInCircleRadius) / 2;
         mArcLeft = mCircleX - mOutCircleRadius + ds;
         mArcTop = mCircleY - mOutCircleRadius + ds;
@@ -74,16 +88,67 @@ public class ProgressRoundView extends View {
         canvas.drawCircle(mCircleX, mCircleY, mOutCircleRadius, mOutCirclePaint);
         canvas.drawArc(mArcRectF, mStartAngle, mSweepAngle, false, mArcPaint);
         canvas.drawCircle(mCircleX, mCircleY, mInCircleRadius, mInCirclePaint);
-
+        canvas.drawText(mProgressText, mCircleX - (mTextRect.right - mTextRect.left) / 2, mCircleY, mTextPaint);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = 0;
+        int height = 0;
+        int mode = MeasureSpec.getMode(widthMeasureSpec);
+        int size = MeasureSpec.getSize(widthMeasureSpec);
+        if (mode == MeasureSpec.EXACTLY) {
+            width = size;
+        } else {
+            width = getWidth() + getPaddingLeft() + getPaddingRight();
+            if (mode == MeasureSpec.AT_MOST) {
+                width = Math.min(width,size);
+            }
+        }
+
+         mode = MeasureSpec.getMode(heightMeasureSpec);
+         size = MeasureSpec.getSize(heightMeasureSpec);
+        if (mode == MeasureSpec.EXACTLY) {
+            height = size;
+        } else {
+            height = getHeight() + getPaddingTop() + getPaddingBottom();
+            if (mode == MeasureSpec.AT_MOST) {
+                height = Math.min(height,size);
+            }
+        }
+
+        setMeasuredDimension(width,height);
     }
+
+
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    public void increateProgress() {
+        mSweepAngle++;
+        if (mSweepAngle <= 360) {
+            mProgressText = (int) (mSweepAngle / 360 * 100) + "%";
+            mTextPaint.getTextBounds(mProgressText, 0, mProgressText.length(), mTextRect);
+            invalidate();
+        }
+    }
+
+    public void setMaxProgress(int maxProgress) {
+        mMaxProgress = maxProgress;
+    }
+
+
+    public void setProgress(float progress) {
+        this.mSweepAngle = progress * (360 / mMaxProgress) + 360 % mMaxProgress;
+        if (progress <= mMaxProgress) {
+            mProgressText = (int) (progress / mMaxProgress * 100) + "%";
+            mTextPaint.getTextBounds(mProgressText, 0, mProgressText.length(), mTextRect);
+//            invalidate();
+            postInvalidate();
+        }
     }
 }
